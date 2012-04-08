404caa5a6972b1797fc8fb365ad0092d948d72b1
Andrew Anderson <picasandrew@gmail.com>
2012-04-08 16:11:27 -0400

    first commit

---
+++ WunderNodeClient.js
@@ -0,0 +1,15 @@
+// 
+// Wunderground Client
+// Andrew Anderson
+// twitter: evalcrux
+//
+
+var QS = require('querystring')
+var HTTP = require('http')
+
+var WunderNode(apikey, debug){
+	var client = this;
+	
+	
+	
+};
