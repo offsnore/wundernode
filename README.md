eea8d37e276a3fea3304cc1d51e99d1974be68d1
Andrew Anderson <picasandrew@gmail.com>
2012-04-14 19:16:34 -0400

    v0.0.1

---
+++ README.md
@@ -0,0 +1,20 @@
+wundernode
+-------
+NodeJS API to integrate with [Wunderground API](http://www.wunderground.com/weather/api/).
+
+This library provides NodeJS with an abstraction to the [Untappd API](http://untappd.com/api/docs/v3) allowing developers to query and integrate Untappd services into their own applications.
+
+This library nor the authors have any formal relationship with Wunderground other than the beer we drink.
+
+## Getting Started
+
+ 1. If you have never done so, sign up for [Wunderground] Dev API service(http://www.wunderground.com/weather/api/) 
+ 2. Wait for you API key to be generated. This should be quick
+ 3. Download wundernode: `npm install wundernode`
+ 4. Look at the Example. Replace the null apikey value with your api key.
+ 5. Play with the example. Good stuff, praise node some and the wunderground developers for opening a nice JSON API.
+
+## Wunderground API format
+- http://api.wunderground.com/api/KEY/FEATURE/[FEATURE]/q/QUERY.FORMAT
+	
+	
--- WunderNodeClient.js
+++ WunderNodeClient.js
@@ -1,15 +1,91 @@
 // 
-// Wunderground Client
+// Wunderground API Client for node.js
 // Andrew Anderson
 // twitter: evalcrux
 //
 
-var QS = require('querystring')
-var HTTP = require('http')
 
-var WunderNode(apikey, debug){
-	var client = this;
+var request = require('request');
+
+var WunderNodeClient = function(apikey, debug) {
+	var that = this;
+	var format = ".json";
+	console.log ('WunderNodeClient initialized, apikey: ' + apikey + ', debug enbaled: ' + debug);
+	var host = 'http://api.wunderground.com/api/' + apikey;
+	
+	if (debug) console.log('Host: ' + host);
+	var get = function(callback, params, path) {
+		var url = host + path;
+		if (debug) console.log ('get: ' + url);
+		request(url, function (error, response, body) {
+  			if (!error && response.statusCode == 200) {
+    			if (debug) console.log('response body: ' + body);
+				callback.call(that, error, body);
+			}
+			else if (error) {
+				console.log('error: '  + err);
+			}
+			
+		});
+	};
+	
+	// autocomplete
+	that.autocomplete = function(callback, query) {
+		var path = "/autocomplete/q/" + query +  format;
+		get(callback, null, path);
+	};
+	
+	// Weather station lookups
+	
+	that.geolookup = function(callback, query) {
+		var path = "/geolookup/q/" + query +  format;
+		get(callback, null, path);
+	};	
+	
+	// Forecasts, current conditions and projected
+	
+	that.hourly7day = function(callback, query) {
+		var path = "/hourly7day/q/" + query +  format;
+		get(callback, null, path);
+	};	
+	
+	that.hourly10day = function(callback, query) {
+		var path = "/hourly10day/q/" + query +  format;
+		get(callback, null, path);
+	};	
+	
+	that.conditions = function(callback, query) {
+		var path = "/conditions/q/" + query +  format;
+		get(callback, null, path);
+	};	
+	
+	that.forecast = function(callback, query) {
+		var path = "/forecast/q/" + query +  format;
+		get(callback, null, path);
+	};
+	
+	that.almanac = function(callback, query) {
+		var path = "/almanac/q/" + query +  format;
+		get(callback, null, path);
+	};
+	
+	that.hourly = function(callback, query) {
+		var path = "/hourly/q/" + query +  format;
+		get(callback, null, path);
+	};
+	
+	that.yesterday = function(callback, query) {
+		var path = "/yesterday/q/" + query +  format;
+		get(callback, null, path);
+	};
 	
 	
+	// Satellite Imagery
 	
+	that.satellite = function(callback, query) {
+		var path = "/satellite/q/" + query +  format;
+		get(callback, null, path);
+	}		
 };
+
+module.exports = WunderNodeClient;
