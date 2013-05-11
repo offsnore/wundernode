72d5df6794873e4bbbc38f863a38e76579de2a64
Andrew Anderson <picasandrew@gmail.com>
2013-05-11 01:31:33 -0400

    Added rate limiter to control API request limits to wunderground AP I
    node module 'limter' added

--- Example.js
+++ Example.js
@@ -9,7 +9,7 @@
 // Changes by lgriffin include defintion of country and city
 // Imports
 
-var WunderNodeClient = require("wundernode", true);
+var WunderNodeClient = require("wundernode");
 var URL = require('url');
 // Definitions
 
@@ -19,10 +19,10 @@ var country = "IE";
 var city = "Waterford";
 
 // Set to true if you want to see all sort of nasty output on stdout.
-var debug = true;
+var debug = false;
 
 // Create Client
-var wunder = new WunderNodeClient(apikey,debug);
+var wunder = new WunderNodeClient(apikey, debug,  10, 'minute');
 
 var express = require('express');
 
@@ -35,33 +35,45 @@ app.get('/', function(req, res) {
 
 
 app.get('/conditions', function(req, res){
-    wunder.conditions(req.query.loc, function(err, obj) {
+   var query = URL.parse(req.url).query;
+   console.log('query: ' +  query);
+    wunder.conditions(query, function(err, obj) {
             if (err){
                     console.log('errors: ' + err);
+                    res.end("Error processing query string:" + queryData.query);
             }
             res.end(obj);
     });
 });
 
 app.get('/almanac', function(req, res){
-    wunder.almanac(req.query, function(err, obj) {
+    var query = URL.parse(req.url).query;
+    wunder.almanac(query, function(err, obj) {
             res.end(obj);
     });
 });
 
 app.get('/forecast', function(req, res){
-   console.log('getting forecast for loc: ' + req.query.log); 
-   
-    wunder.forecast(req.query.loc, function(err, obj) {
+    var query = URL.parse(req.url).query;
+    wunder.forecast(query, function(err, obj) {
             res.end(obj);
     });
 });
 
+app.get('/forecast10day', function(req, res) {
+   var query = URL.parse(req.url).query;
+   console.log('getting forecast for loc: ' + query); 
+   
+    wunder.forecast10day(req.query.loc, function(err, obj) {
+         res.end(obj);
+    });
+});
+
 app.get('/geolookup', function(req, res) {
-   var queryData = URL.parse(req.url);
+   var query = URL.parse(req.url).query;
    console.log('finding location for geoloc: ' + JSON.stringify(queryData));
    
-   wunder.geolookup(queryData.query, function(err, obj) {
+   wunder.geolookup(query, function(err, obj) {
             res.end(obj);
     });
    
@@ -75,38 +87,56 @@ app.get('/hourly', function(req, res){
 });
 
 app.get('/hourly7day', function(req, res){
-    var queryData = URL.parse(req.url);
-    wunder.hourly7day(queryData.query, function(err, obj) {
+    var query = URL.parse(req.url).query;
+    wunder.hourly7day(query, function(err, obj) {
             res.end(obj);
     });
 });
 
 app.get('/hourly10day', function(req, res){
-    var queryData = URL.parse(req.url);
-    wunder.hourly10day(queryData.query, function(err, obj) {
+    var query = URL.parse(req.url).query;
+    wunder.hourly10day(query, function(err, obj) {
             res.end(obj);
     });
 });
 
 app.get('/yesterday', function(req, res){
-    var queryData = URL.parse(req.url);
-    wunder.yesterday(queryData.query, function(err, obj) {
+    var query = URL.parse(req.url).query;
+    wunder.yesterday(query, function(err, obj) {
+            res.end(obj);
+    });
+});
+
+app.get('/planner', function(req, res){
+    var queryData = URL.parse(req.url, true);
+   console.log('queryData.range: ' + queryData.range);
+   console.log('queryData.query: ' + queryData.query);
+   var query = queryData.query;
+   var range = queryData.range;
+   if (!query || !range) {
+      res.send(400, 'Required parameters missing, invalid query');
+      res.end();
+   }
+   console.log('queryData: ' + JSON.stringify(queryData));
+    wunder.planner(query, range, function(err, obj) {
             res.end(obj);
     });
 });
 
 app.get('/satellite', function(req, res){
-    var queryData = URL.parse(req.url);
-    wunder.satellite(queryData.query, function(err, obj) {
+    var query = URL.parse(req.url).query;
+    wunder.satellite(query, function(err, obj) {
             res.end(obj);
     });
 });
 
-app.get('/foo', function(req, res){
-    wunder.foo(req.query.loc, function(err, obj) {
-            res.end(obj);
+app.get('/webcams', function(req, res){
+    var query = URL.parse(req.url).query;
+    wunder.webcams(query, function(err, obj) {
+      res.end(obj);
     });
 });
+
 
 
 app.listen(3000);
